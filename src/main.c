eb1e245ed2b198e22026910634145b44dc6c6e43
jack <electricalmemory83720x0@protonmail.com>
2023-08-01 14:33:56 +0100

    Change to do some profiling

--- src/main.c
+++ src/main.c
@@ -7,17 +7,13 @@
 
 int main(int argc, char  *argv[])
 {
-    // char * file_path = argv[1];
-    char * file_path = "test";
+    char * file_path = argv[1];
+    // char * file_path = "test";
     FILE* f = fopen(file_path, "rb");
     uint64_t file_size = get_file_size(file_path);
     // printf("%d\n", get_file_size(file_path));
     printf("\nHEX OUTPUT\n");
     dump_bin(f, 1, 5, true, file_size, D_HEXADECIMAL);
-    printf("\nOCTAL OUPUT\n");
-    dump_bin(f, 1, 5, true, file_size, D_OCTAL);
-    printf("\nBINARY OUPUT\n");
-    dump_bin(f, 1, 5, true, file_size, D_BINARY);
 
     return 0;
 }
